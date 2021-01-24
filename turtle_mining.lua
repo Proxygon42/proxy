2f426c1ffbda2e75b76f0ebad78da22018adc69c
Proxygon42 <nils.christian12@gmail.com>
2021-01-24 09:23:00 +0100

    drop_inventory_chest_error count_chests

--- turtle_mining.lua
+++ turtle_mining.lua
@@ -6,9 +6,9 @@ function set_parameter()
     maxFuel = turtle.getFuelLimit()--20.000 Bei normalen Turtles
     height = 0
 	minFuelAmount = 200
-    selector_coal = 1 --Wenn auf Slot 1 nichts gefunden wird, wird durch die funktion select_coal() der Parameter select_coal angepasst
-    --Direction wird benötigt um am Ende der Row anzugeben, ob sich rechts oder links bewegt werden soll:
-    direction = 1 --1 steht für rechts und -1 für links
+    coal_string = "minecraft:coal"
+    chest_string = "minecraft:chest"--TODO ender chest einrichten
+    comming_from = "back"--Letzte Richtung aus der Die Turtle gekommen ist["back","forward","up","down"]
 end
 
 ---DIALOG:---
@@ -37,55 +37,51 @@ end
 ---FUELING:---
 function checker_fuel()
     --Dieser Code bezieht sich auf Kohle Energie
-    local i_checker_fuel = 0
     while turtle.getFuelLevel() <= minFuelAmount do
         --Solange minFuelAmount nicht übertroffen wurde, wird diese Schleife wiederholt.
-        if select_coal() ~= nil then
-            --Wenn Kohle gefunden wurde
-            while select_coal() ~= nil do
-                --Es wird solange das inv geleert, bis maxFuel erreicht wurde oder keine Kohle mehr gefunden wurde
-                --Zähler um zu wissen, ob es der erste Durchgang ist.
-                i_checker_fuel = i_checker_fuel + 1
-                
-                slot_coal = select_coal()--Gibt einen Slot von Kohle wieder oder nil, wenn keine Kohle gefunden wurde.
-                coal_amount = turtle.getItemCount(slot_coal)
-                if i_checker_fuel == 1 then
-                    --Bei dem ersten Durchgang, wird 1 Kohle übrig gelassen, damit immer ein Slot für Kohle freigehalten wird.
-                    coal_amount = coal_amount - 1
-                end
-                -- Max Anzahl an Items beachten, um nicht mehr als max möglich zu fuelen:
-                maxFuel_items = math.floor((maxFuel-turtle.getFuelLevel())/fuel_value)
-                if coal_amount > maxFuel_items then
-                    coal_amount = maxFuel_items
-                end
-
-                --Refuel:
-                turtle.refuel(coal_amount)
-            end
-
-        else
+        
+        --Die function turtle_refuel() wird aufgerufen, wodurch refueled wird, aber im Falle fehlender Kohle nil ausgegeben wird:
+        if turtle_refuel() == nil then
             --Wenn keine Kohle gefunden wurde
             print("Keine Kohle gefunden. Bitte mit Kohle befüllen.")
             print("Drücken Sie ENTER, wenn sie Kohle eingeführt haben.")
             read()
+        else
+            --Gefueled
+            print("Turtle goes brrr")
         end
-        
     end
-    --Gefueled
-    print("Turtle goes brrr")
 end
-function select_coal()
-	--sucht und selected kohle
-	--int select_coal ->siehe set_parameter()
-	for i_select_coal = 1 , 16 , 1 do
-		local item_data = turtle.getItemDetail(i_select_coal)
-		if item_data.name == "minecraft:coal" then--PARAMETER
-			return i_select_coal
-			--Die For Schleife wird bei gefundener Kohle abgebrochen und select_kohle wird die Slotnummer mitgegeben, welche die Kohle beinhaltet.
-		end
+
+function turtle_refuel()
+    if select_item(coal_string)[1] ~= nil then
+        --Wenn Kohle gefunden wurde
+        local i_checker_fuel = 1
+        while select_item(coal_string)[i_checker_fuel] ~= nil do
+            --Es wird solange das inv geleert, bis maxFuel erreicht wurde oder keine Kohle mehr gefunden wurde
+            
+            
+            slot_coal = select_item(coal_string)[i_checker_fuel]--Gibt einen Slot von Kohle wieder oder nil, wenn keine Kohle gefunden wurde.
+            coal_amount = turtle.getItemCount(slot_coal)
+            if i_checker_fuel == 1 then
+                --Bei dem ersten Durchgang, wird 1 Kohle übrig gelassen, damit immer ein Slot für Kohle freigehalten wird.
+                coal_amount = coal_amount - 1
+            end
+            -- Max Anzahl an Items beachten, um nicht mehr als max möglich zu fuelen:
+            maxFuel_items = math.floor((maxFuel-turtle.getFuelLevel())/fuel_value)
+            if coal_amount > maxFuel_items then
+                coal_amount = maxFuel_items
+            end
+
+            --Refuel:
+            turtle.refuel(coal_amount)
+            --Zähler um zu wissen, ob es der erste Durchgang ist:
+            i_checker_fuel = i_checker_fuel + 1
+        end
+
+    else
+        return nil
     end
-    return nil
-	
 end
 
 ---MINING/MOVING:---
@@ -111,6 +107,7 @@ function route_mine()
         --Die Breite gibt die Reihen rechts von der Turtle abzubauen.
         x_direction = 1
     end
+
     for i_x = 1 , x_forloop , 1 do
         --Die Breite, welche x ist, bestimmt wie viele Reihen gegangen werden.
 
@@ -136,6 +133,7 @@ function route_mine()
                     --Erster Durchgang wird gerade aus gegangen:
                     turtle.dig()
                     turtle.forward()
+                    comming_from = "back"
                 else
                     --Es wird hoch oder runter gegangen
                     if y_direction == 1 then
@@ -143,13 +141,17 @@ function route_mine()
                         turtle.digUp()
                         turtle.up()
                         y_direction = -1
+                        comming_from = "down"
                     elseif y_direction == -1 then
                         --Runter:
                         turtle.digDown()
                         turtle.down()
                         y_direction = 1
+                        comming_from = "up"
                     end
                 end
+                --Überprüft, ob nach dem abbauen noch PLatz im Inventar für neue Items ist:
+                inventory_space()
             end
         end
         if i_x ~= x_xyz then
@@ -170,15 +172,119 @@ function turtle_turn(direction)
     end
 end
 
-function checker_inventory()
-    --Checkt ob mindestens ein Slot frei ist.
+function select_item(item)
+    --sucht gibt in einer table alle Slots wieder, wo das Item gefunden wird.
+    slot_table = {}
+	for i_select = 1 , 16 , 1 do
+		local item_data = turtle.getItemDetail(i_select)
+		if item_data.name == item then
+			table.insert(slot_table, i_select)
+		end
+    end
+    return slot_table
+end
+
+function inventory_space()
+    if checker_empty_slots()[1] == nil then
+        --Wenn kein freier Slot gefunden wurde, wird das Inventar in eine chest entleert
+        chest_place()
+    end
+end
+
+function checker_empty_slots()
+    --Gibt in einem table die leeren slot numbers
+    local free_slots = {}
+    for i_slot = 1 , 16 , 1 do
+        if turtle.getItemCount(i_slot) == 0 then
+            --Wenn ein freier Slot gefunden wird;
+            table.insert(free_slots, i_slot)
+            
+        end
+    end
+    return free_slots
+end
+
+
+
+function chest_place()
+    --TODO für enderchest anpassen + print anpassen
     --Wenn keine Chest im Inventory ist, wird geprintet "Bitte entleeren sie das Inventar" oder so.
+    local selector_chest = select_item(chest_string)[1]
+    while selector_chest == nil do
+        print("Das Inventar der turtle ist voll.")
+        print("Bitte entleeren sie das Inventar oder geben Sie eine Chest dem Inventar hinzu.")
+        print("Drücken Sie danach ENTER zum fortfahren.")
+        read()
+        selector_chest = select_item(chest_string)[1]
+    end
+    --Selects the Chest:
+    turtle.select(selector_chest)
+    
+    if comming_from == "back" then
+        turtle.turnLeft()
+        turtle.turnLeft()
+        turtle.place()
+        drop_inventory_chest()
+        turtle.dig()
+        turtle_turnRight()
+        turtle_turnRight()
+    elseif comming_from == "up" then
+        turtle.placeUp()
+        drop_inventory_chest()
+        turtle.digUp()
+    elseif comming_from == "down" then
+        turtle.placeDown()
+        drop_inventory_chest()
+        turtle.digDown()
+    end
+
 
 end
 
-function chest_place()
-    
+function drop_inventory_chest()
+    --Es wird zunächst alle Mögliche Kohle bis auf 1 zum refuelen verwendet:
+    turtle_refuel()
 
+    for i = 1, 16, 1 do
+
+        turtle.select(i)
+        if turtle.getItemDetail(i).name ~= coal_string and turtle.getItemDetail(i).name ~= chest_string then
+                --Prüfung, ob eine Chest bereit steht
+            if comming_from == "back" then
+                if turtle.inspect().name ~= chest_string then
+                    drop_inventory_chest_error(i)
+                end
+            elseif comming_from == "up" then
+                if turtle.inspectUp().name ~= chest_string then
+                    drop_inventory_chest_error(i)
+                end
+            elseif comming_from == "down" then
+                if turtle.inspectDown().name ~= chest_string then
+                    drop_inventory_chest_error(i)
+                end
+            end
+
+            if turtle.drop() == false then
+                drop_inventory_chest_error(i)
+            end
+        end
+
+        
+        
+
+
+    end
+end
+
+function drop_inventory_chest_error(slot)
+    if checker_empty_slots()[2] == nil then
+        local count_chests = turtle.getItemCount(select_item(chest_string))
+        if count_chests == 64 or count_chests == 0 then
+            --Wenn nicht mindestens 1 Slot frei ist UND im Inventar in keinem vielleicht vorhandenen Chest-Stack noch Platz frei ist:
+            
+        end
+
+    end
 end
 
 --Start:
