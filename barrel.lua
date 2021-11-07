9181f541634e1f3e984efc9f050c0eef0639b112
Proxygon42 <nils.christian12@gmail.com>
2021-11-07 01:26:39 +0100

    split in two functions. The break should now prevent that more then one field is recognized as the infinite_barrel

--- barrel.lua
+++ barrel.lua
@@ -7,20 +7,26 @@ function barrel()
     repeat
         turtle.suck()
     until turtle.suck() == false
-    
+
+    inner_barrel()
+
+    turtle.select(9)
+    turtle.dig()
+end
+
+function inner_barrel()
     for i_barrel = 1, 16, 1 do
         turtle.select(i_barrel)
         local item_detail = turtle.getItemDetail()
         if item_detail ~= nil then
             if item_detail.name == barrel_string then
                 i_infinite_barrel = i_barrel
+                break
             else
                 turtle.dropUp(i_barrel)
             end
         end
     end
-    turtle.select(9)
-    turtle.dig()
 end
 
 while true do
